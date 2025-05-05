abe6a6d5fd43f527cb810195bf0f098d99ffc863
Harrison Grias <harrison.grias@gmail.com>
2025-05-05 15:05:49 -0500

    refactor: switch font to roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import { Spotlight } from "./components/Spotlight";
 import "./globals.css";
 
-const inter = Inter({
+const roboto = Roboto({
   subsets: ["latin"],
   variable: "--font-sans",
   display: "swap",
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang={`${inter.variable} en scroll-smooth`}>
+    <html lang={`${roboto.variable} en scroll-smooth`}>
       <body
         className={`text-foreground bg-background selection:text-primary min-h-screen w-full font-sans leading-relaxed antialiased selection:bg-yellow-200/10`}
       >
